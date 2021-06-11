cfef5afa28d78c231871960df211a685593bd97c
854950 <854950@SAIT227688IT.ACDM.DS.SAIT.CA>
2021-06-11 11:50:18 -0600

    Fifth commit Added some comments in NoteServlet

--- src/java/servlets/NoteServlet.java
+++ src/java/servlets/NoteServlet.java
@@ -18,16 +18,16 @@ public class NoteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
-        BufferedReader br = new BufferedReader(new FileReader(new File(path)));
-        Note note = new Note(br.readLine(), br.readLine());
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt"); // get note path
+        BufferedReader br = new BufferedReader(new FileReader(new File(path))); // read note path
+        Note note = new Note(br.readLine(), br.readLine()); // create note object and use br.readLine() to read each line of note.txt
         request.setAttribute("note", note);
         // this will display the requested JSP as a view
-        String edit = request.getParameter("edit");
+        String edit = request.getParameter("edit"); // getting parameter of edit from viewnote.jsp
         if (edit != null && edit.equals("true")) {
-            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response);
+            getServletContext().getRequestDispatcher("/WEB-INF/editnote.jsp").forward(request, response); // if edit is clicked on show editnote page
         } else {
-            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response);
+            getServletContext().getRequestDispatcher("/WEB-INF/viewnote.jsp").forward(request, response); // otherwise just show viewnote page
         }
 
     }
@@ -36,18 +36,18 @@ public class NoteServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String title = request.getParameter("note_title");
-        String contents = request.getParameter("contents_title");
+        String title = request.getParameter("note_title"); // creates a string to read note_title text box from editnote.jsp
+        String contents = request.getParameter("contents_title"); // creates a string to read contents_title text box from editnote.jsp
 
-        Note note = new Note(title, contents);
+        Note note = new Note(title, contents); //creates a new note object
         request.setAttribute("note", note);
 
-        String path = getServletContext().getRealPath("/WEB-INF/note.txt");
+        String path = getServletContext().getRealPath("/WEB-INF/note.txt"); // gets the path for note.txt
 
-        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false)));
+        PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter(path, false))); // prints to the path specified
 
-        pw.println(note.getTitle());
-        pw.println(note.getContents());
+        pw.println(note.getTitle()); // writes to the first line
+        pw.println(note.getContents()); // writes to the second line
 
         pw.close(); // always close printwriter
         //display the  JSP
